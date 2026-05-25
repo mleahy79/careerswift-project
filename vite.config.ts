b5a218fdd1edfe494a1a33e4c36bff16c6772c9a
Mitchell Leahy <mleahy79@icloud.com>
2026-05-25 07:13:12 -0700

    add base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/careerswift-project/',
   plugins: [react(), tailwindcss()],
 })
